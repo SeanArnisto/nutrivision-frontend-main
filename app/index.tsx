e5bfb2240d23ef886daaf46d884ea9b13eda3c8a
Sean <arnisto.sean@ue.edu.ph>
2025-08-09 03:41:03 +0800

    feat: bottom navigation bar

--- app/index.tsx
+++ app/index.tsx
@@ -22,7 +22,7 @@ export default function Page2() {
   const navigation = useNavigation<Page2ScreenNavigationProp>();
 
   const handleGetStarted = () => {
-    navigation.navigate("onboarding"); // TODO: confirm route
+    navigation.navigate("page-2"); // TODO: confirm route
   };
 
   const handleLogin = () => {
